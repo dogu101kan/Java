c426be8d8c4ba97db4c5be6988010ad59539c2cd
Doğukan Gazel <57948810+dogu101kan@users.noreply.github.com>
2022-03-23 00:25:40 +0300

    Add files via upload

---
+++ Taksimetre.java
@@ -0,0 +1,23 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+
+        int km ;
+        double perKm = 2.2 , total , startPrice = 10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi km cinsinde gir.");
+        km = input.nextInt();
+
+        total = (km*perKm) + startPrice;
+
+        total = (total < 20) ? 20 :total;
+        System.out.print("Toplam Tutar " + total);
+
+
+
+    }
+}

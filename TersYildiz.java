43e82ffc8c0eef3ff10cf96ecf67c4e6a1024ab5
Doğukan Gazel <57948810+dogu101kan@users.noreply.github.com>
2022-04-04 18:59:03 +0300

    Add files via upload

---
+++ TersYildiz.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TersYildiz {
+    public static void main(String[] args) {
+
+            int a;
+            Scanner input = new Scanner(System.in);
+            a=input.nextInt();
+
+            for (int i =a-1;i>=0;i--){
+                for (int k=0;k<(a-i);k++){
+                    System.out.print(" ");
+                }
+                for (int j=1;j<=(2*i+1);j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+        }
+    }
+
